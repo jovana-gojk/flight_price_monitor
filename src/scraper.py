d983c5a37a9b83e695c7940ccd0e42de6a6a34f4
jovana-gojk <jovana.gojkovic2003@gmail.com>
2026-09-17 16:09:20 +1000

    Added the google flights web scraper in src

---
+++ src/scraper.py
@@ -0,0 +1,134 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime, timedelta
+
+departure_airport = "OOL"
+arrival_airport = "SYD"
+earliest_flight_time = "2:00 PM"
+
+def make_url(origin, destination, date):
+    url = f"https://www.google.com/travel/flights?hl=en&curr=AUD&q=One%20way%20flights%20from%20{origin}%20to%20{destination}%20on%20{date}"
+    return url
+
+def parse_flight(flight):
+
+    flight_link = flight.find(attrs={"role": "link"})
+
+    if not flight_link:
+        return None
+
+    aria_label = flight_link.get("aria-label")
+
+    price = re.search(r"(\d+) Australian dollars", aria_label)
+    airline = re.search(r"flight with (.*?)\. Leaves", aria_label)
+    departure_time = re.search(r"at ([\d:]+)\s*([AP]M)", aria_label)
+    arrival_time = re.search(r"arrives .*? at ([\d:]+)\s*([AP]M)", aria_label)
+    duration = re.search(r"Total duration (.*?)\.", aria_label)
+
+    return {
+        "price": int(price.group(1)) 
+            if price else None,
+
+        "airline": (airline.group(1)
+            if airline else None),
+
+        "departure_time": (departure_time.group(1) + " " + departure_time.group(2)
+            if departure_time else None),
+
+        "arrival_time": (arrival_time.group(1) + " " + arrival_time.group(2)
+            if arrival_time else None),
+
+        "duration": (duration.group(1)
+            if duration else None)
+    }
+
+def get_response_for_flights(flight_day):
+    
+    response = requests.get(
+        flight_day,
+        headers={
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/151.0.0.0 Safari/537.36"
+            )
+        }
+    )
+    return response
+
+def print_flight_info(flights):
+    # Print results
+    for flight in flights:
+        print(
+            f"${flight['price']} | "
+            f"{flight['airline']} | "
+            f"{flight['departure_time']}"
+        )
+
+def get_sorted_flights(flight_day, earliest_flight_time):
+    response = get_response_for_flights(flight_day)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    flights = soup.find_all("li", class_="pIav2d")
+
+    flight_data = []
+
+    for flight in flights:
+
+        data = parse_flight(flight)
+
+        if data:
+            flight_data.append(data)
+
+    unique_flights = []
+
+    for flight in flight_data:
+
+        if flight not in unique_flights:
+            unique_flights.append(flight)
+
+    flight_data = unique_flights
+
+    # Convert earliest flight time to a time object
+    earliest_time = datetime.strptime(earliest_flight_time,"%I:%M %p").time()
+
+
+    # Filter flights after the earliest allowed time
+    eligible_flights = []
+
+    for flight in flight_data:
+        departure_time = datetime.strptime(flight["departure_time"],"%I:%M %p").time()
+
+        if departure_time >= earliest_time:
+            eligible_flights.append(flight)
+
+
+    # Sort eligible flights by price
+    eligible_flights.sort(key=lambda flight: flight["price"])
+
+    print_flight_info(eligible_flights)
+
+start_friday = datetime(2026, 9, 18)
+
+number_of_weekends = 5
+
+for i in range(number_of_weekends):
+
+    friday = start_friday + timedelta(weeks=i)
+    sunday = friday + timedelta(days=2)
+
+    friday_date = friday.strftime("%Y-%m-%d")
+    sunday_date = sunday.strftime("%Y-%m-%d")
+
+    friday_url = make_url(departure_airport, arrival_airport, friday_date)
+    sunday_url = make_url(arrival_airport, departure_airport, sunday_date)
+
+    print("\n==============================")
+    print(f"Weekend: {friday_date} → {sunday_date}")
+    print("==============================")
+
+    print("Friday flights:\n")
+    get_sorted_flights(friday_url, earliest_flight_time)
+    print("\nSunday flights:\n")
+    get_sorted_flights(sunday_url, earliest_flight_time)
